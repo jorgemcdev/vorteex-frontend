d6664d9746d5c4594cd72707b38c2143001a04c7
Jorge Carvalho <jorgecarvalhodpt@gmail.com>
2019-02-05 00:40:54 +0000

    Add logout action to NavBar

--- src/components/shared/navbar/NavBar.js
+++ src/components/shared/navbar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import {
   Collapse,
@@ -7,70 +8,74 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+  NavLink
 } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+// Import Actions
+import { logoutRequest } from '../../../actions';
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
-
-    this.toggle = this.toggle.bind(this);
     this.state = {
       isOpen: false
     };
   }
 
-  toggle() {
+  toggle = () => {
     this.setState(prevState => ({
       isOpen: !prevState.isOpen
     }));
   }
 
+  handleLogout = (e) => {
+    e.preventDefault();
+    const { logout } = this.props;
+    logout();
+  }
+
   render() {
     const { isOpen } = this.state;
+    const { isAuthenticated } = this.props;
     return (
       <div>
         <Navbar color="light" light expand="md">
-
           <NavbarToggler onClick={this.toggle} />
           <NavbarBrand tag={Link} to="/">
             Vorteex
           </NavbarBrand>
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
+          {isAuthenticated
+            && (
+              <Collapse isOpen={isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="#" onClick={this.handleLogout}>Logout</NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            )}
         </Navbar>
       </div>
     );
   }
 }
 
-export default NavBar;
+NavBar.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  logout: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  user: state.auth.user
+});
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logoutRequest())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NavBar);
