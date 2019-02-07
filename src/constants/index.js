ce55a77a6c9d0de27779aa8d5c97e3750a8295f7
Jorge Carvalho <jorgecarvalhodpt@gmail.com>
2019-02-07 21:48:02 +0000

    Add Constants for: Bots, Rooms

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,16 @@ import * as auth from './core/auth';
 import * as message from './core/message';
 import * as modal from './core/modal';
 
+// App
+import * as bots from './bots';
+import * as rooms from './rooms';
+
 export {
   // Core
   auth,
   message,
-  modal
+  modal,
+  // App
+  bots,
+  rooms
 };
