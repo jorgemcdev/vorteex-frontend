f85747a1121e0c0c6b160c082a059c460b9cae27
Jorge Carvalho <jorgecarvalhodpt@gmail.com>
2019-02-07 19:38:08 +0000

    Add routes: /bots /rooms /rooms-association / templates

--- src/layouts/default/routes.js
+++ src/layouts/default/routes.js
@@ -1,16 +1,40 @@
 import React from 'react';
 
 const Dashboard = React.lazy(() => import('../../components/dashboard/Dashboard'));
+const Bots = React.lazy(() => import('../../components/bots/Bots'));
+const Rooms = React.lazy(() => import('../../components/rooms/Rooms'));
 const Graphs = React.lazy(() => import('../../components/graphs/Graphs'));
+const Templates = React.lazy(() => import('../../components/templates/Templates'));
+const RoomsAssociation = React.lazy(() => import('../../components/rooms-association/RoomsAssociation'));
 
-// https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
+const Page404 = React.lazy(() => import('../../components/shared/Pages/Page404'));
+const Page500 = React.lazy(() => import('../../components/shared/Pages/Page500'));
+
 const routes = [
   {
-    id: 1, path: '/', exact: true, name: 'Dashboard', component: Dashboard
+    id: 0, path: '/', exact: true, name: 'Dashboard', component: Dashboard
   },
   {
-    id: 2, path: '/graphs', exact: true, name: 'Graphs', component: Graphs
+    id: 1, path: '/bots', exact: true, name: 'Bots', component: Bots
   },
+  {
+    id: 2, path: '/rooms', exact: true, name: 'Rooms', component: Rooms
+  },
+  {
+    id: 3, path: '/graphs', exact: true, name: 'Graphs', component: Graphs
+  },
+  {
+    id: 4, path: '/templates', exact: true, name: 'Templates', component: Templates
+  },
+  {
+    id: 5, path: '/rooms-association', exact: true, name: 'Rooms Associations', component: RoomsAssociation
+  },
+  {
+    id: 98, path: '/500', exact: true, name: '500', component: Page500,
+  },
+  {
+    id: 99, path: '/*', exact: true, name: '404', component: Page404,
+  }
 ];
 
 export default routes;
