a78cceb3e923463542fdf92bb0030bad9fae03a4
Jorge Carvalho <jorgecarvalhodpt@gmail.com>
2019-02-08 16:33:02 +0000

    Update Graph

--- src/components/graphs/Graphs.js
+++ src/components/graphs/Graphs.js
@@ -1,39 +1,41 @@
 import React, { Component } from 'react';
 import Graph from 'vis-react';
+import axios from 'axios';
+
+// Data Parser
+import BotsDataParser from '../../lib/botsDataParser';
+
+// Graph Style
 import './graphs.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class Graphs extends Component {
-  render() {
-    const nodes = [{"id": 10001, "group": 0, "label": "CleanMX"},
-                   {"id": 20001, "group": 1, "label": "CleanMX Reports"},
-                   {"id": 10002, "group": 0, "label": "CleanMX"},
-                   {"id": 20002, "group": 1, "label": "CleanMX Events"},
-                   {"id": 10003, "group": 0, "label": "PhishTank"},
-                   {"id": 20003, "group": 1, "label": "PhishTank Reports"},
-                   {"id": 10004, "group": 0, "label": "PhishTank"},
-                   {"id": 20004, "group": 1, "label": "PhishTank Events"},
-                   {"id": 10005, "group": 0, "label": "Deduplicator"},
-                   {"id": 20005, "group": 1, "label": "Deduplicated Events"},
-                   {"id": 10006, "group": 0, "label": "MongoDB"},
-                   {"id": 10007, "group": 0, "label": "ElasticSearch"}];
-
-    const edges = [{"from": 20001, "to": 10001},
-                   {"from": 20001, "to": 10002},
-                   {"from": 20002, "to": 10002},
-                   {"from": 20003, "to": 10003},
-                   {"from": 20003, "to": 10004},
-                   {"from": 20004, "to": 10004},
-                   {"from": 20002, "to": 10005},
-                   {"from": 20004, "to": 10005},
-                   {"from": 20005, "to": 10005},
-                   {"from": 20005, "to": 10006},
-                   {"from": 20005, "to": 10007}];
-
-    const graph = {
-      nodes,
-      edges
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: [],
+      edges: []
     };
+  }
+
+  async componentDidMount() {
+    const { data } = await axios({
+      method: 'get',
+      url: 'http://vorteex.ovnisec.com:9000/bots/',
+      headers: { 'Content-Type': 'application/json' },
+      timeout: 5000
+    });
+    const g = new BotsDataParser();
+    const result = await g.parseData(data);
+    this.setState({
+      nodes: result.nodes,
+      edges: result.edges
+    });
+  }
+
+  render() {
+    const { nodes, edges } = this.state;
+
+    const graph = { nodes, edges };
 
     const options = {
       layout: {
@@ -54,15 +56,16 @@ class Graphs extends Component {
       },
       physics: {
         enabled: false,
-        minVelocity: 0.75
+        minVelocity: 0
       }
     };
 
     const events = {
+      /*
       select(event) {
-        // var { nodes, edges } = event;
-        console.log('Nodes:', event);
+        var { nodes, edges } = event;
       }
+      */
     };
 
     return (
