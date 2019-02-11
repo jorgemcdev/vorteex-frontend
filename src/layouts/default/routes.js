da8a0eff1fe7f82f376795a5748173cbf84d2028
Jorge Carvalho <jorgecarvalhodpt@gmail.com>
2019-02-11 02:56:09 +0000

    Add Instances Routes: List, Edit and New

--- src/layouts/default/routes.js
+++ src/layouts/default/routes.js
@@ -1,22 +1,44 @@
 import React from 'react';
 
 const Dashboard = React.lazy(() => import('../../components/dashboard/Dashboard'));
+
+// Instances
 const Instances = React.lazy(() => import('../../components/instances'));
+const InstancesNew = React.lazy(() => import('../../components/instances/forms/FormNew'));
+const InstancesEdit = React.lazy(() => import('../../components/instances/forms/FormEdit'));
+
+// Templates
 const Templates = React.lazy(() => import('../../components/templates'));
+
+// TemplateGroups
 const TemplatesGroups = React.lazy(() => import('../../components/templates-groups'));
+
+// Modules
 const Modules = React.lazy(() => import('../../components/modules'));
+
+// Rooms
 const Rooms = React.lazy(() => import('../../components/rooms'));
 
+// Pages
 const Page404 = React.lazy(() => import('../../components/shared/Pages/Page404'));
 const Page500 = React.lazy(() => import('../../components/shared/Pages/Page500'));
 
 const routes = [
+  // Dashboard
   {
     id: 0, path: '/', exact: true, name: 'Dashboard', component: Dashboard
   },
+  // Instances
   {
     id: 1, path: '/instances', exact: true, name: 'Instances', component: Instances
   },
+  {
+    id: 11, path: '/instances/new', exact: true, name: 'New', component: InstancesNew
+  },
+  {
+    id: 12, path: '/instances/edit/:id', exact: true, name: 'Edit', component: InstancesEdit
+  },
+  // Templates
   {
     id: 2, path: '/templates', exact: true, name: 'Templates', component: Templates
   },
@@ -33,7 +55,7 @@ const routes = [
     id: 98, path: '/500', exact: true, name: '500', component: Page500,
   },
   {
-    id: 99, path: '/*', exact: true, name: '404', component: Page404,
+    id: 99, path: '/404', exact: true, name: '404', component: Page404,
   }
 ];
 
