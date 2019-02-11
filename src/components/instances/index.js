1d256078643de5cea541f560a011135a5a23a5c2
Jorge Carvalho <jorgecarvalhodpt@gmail.com>
2019-02-11 02:56:45 +0000

    Instances CRUD Sample components

--- src/components/instances/index.js
+++ src/components/instances/index.js
@@ -1,81 +1,13 @@
-/* eslint-disable no-console */
 import React from 'react';
-import {
-  Card, CardBody, CardTitle, CardFooter, CardHeader,
-  Col, Form, FormGroup, Label, Row, Button, ButtonGroup, Input
-} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-const Bots = () => {
-  console.log('bot form');
-  return (
-    <Row>
-      <Col md="6">
-        <Card>
+const Instances = () => (
+  <div>
+    <p>List Instances</p>
+    <p><Link to="/instances/edit/1">Edit</Link></p>
+    <p><Link to="/instances/new">New</Link></p>
+  </div>
 
-          <CardHeader className="bg-white">
-            <CardTitle>
-              <strong>BOTS</strong>
-              {' '}
-              <small>FORM</small>
-            </CardTitle>
-          </CardHeader>
+);
 
-          <CardBody>
-            <Form>
-              <FormGroup row>
-                <Label for="template" sm={2}>Template</Label>
-                <Col sm={10}>
-                  <Input type="select" name="template" id="template">
-                    <option />
-                    <option>Template 1</option>
-                    <option>Template 2</option>
-                    <option>Template 3</option>
-                    <option>Template 4</option>
-                    <option>Template 5</option>
-                  </Input>
-                </Col>
-              </FormGroup>
-
-              <FormGroup row>
-                <Label for="name" sm={2}>Name</Label>
-                <Col sm={10}>
-                  <Input type="text" name="name" id="name" placeholder="" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup row>
-                <Label for="codename" sm={2}>Code Name</Label>
-                <Col sm={10}>
-                  <Input type="text" name="codename" id="codename" placeholder="" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup row>
-                <Label for="group" sm={2}>Group</Label>
-                <Col sm={10}>
-                  <Input type="text" name="group" id="group" placeholder="" />
-                </Col>
-              </FormGroup>
-            </Form>
-          </CardBody>
-
-          <CardFooter className="bg-white">
-            <Row>
-              <Col>
-                <Button color="secondary">Reset</Button>
-              </Col>
-              <Col className="text-right">
-                <ButtonGroup>
-                  <Button outline color="primary">Confirm</Button>
-                  <Button outline color="danger">Cancel</Button>
-                </ButtonGroup>
-              </Col>
-            </Row>
-          </CardFooter>
-        </Card>
-      </Col>
-    </Row>
-  );
-};
-
-export default Bots;
+export default Instances;
