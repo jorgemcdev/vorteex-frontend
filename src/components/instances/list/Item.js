17f674de2e6905d8343c54cb94cc61b63c902c27
Jorge Carvalho <jorgecarvalhodpt@gmail.com>
2019-02-28 12:24:11 +0000

    Add Options Menu

--- src/components/instances/list/Item.js
+++ src/components/instances/list/Item.js
@@ -1,33 +1,32 @@
 /* eslint-disable camelcase */
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Badge } from 'reactstrap';
+import { Badge } from 'reactstrap';
 import PropTypes from 'prop-types';
 
-const Item = ({ item, handleDelete }) => {
+import ItemDropDown from './ItemDropDown';
+
+const Item = ({ item, handleDelete, handleEdit }) => {
   const {
     id, name, codename, description, group, template, source_rooms, destination_rooms
   } = item;
 
-  // <Badge color="secondary">New</Badge>
+  const sourceRooms = source_rooms && source_rooms.map(el => <Badge className="mr-1 text-white" color="warning">{el.name}</Badge>);
+  const badges = destination_rooms && <Badge color="success">{destination_rooms.name}</Badge>;
+
   return (
-    <React.Fragment>
-      <tr>
-        <td>{id}</td>
-        <td><Link to={`/instances/edit/${id}`}>{name}</Link></td>
-        <td>{codename}</td>
-        <td>{description}</td>
-        <td>{group}</td>
-        <td>{template.name}</td>
-        <td>{destination_rooms ? <Badge color="success">{destination_rooms.name}</Badge> : ''}</td>
-        <td>{source_rooms && source_rooms.map(el => <Badge className="mr-1 text-white" color="warning">{el.name}</Badge>)}</td>
-        <td width="1%">
-          <Button outline color="danger" size="sm" onClick={() => handleDelete(id)}>
-            <i className="fa fa-minus" />
-          </Button>
-        </td>
-      </tr>
-    </React.Fragment>
+    <tr>
+      <td>{id}</td>
+      <td>{name}</td>
+      <td>{codename}</td>
+      <td>{description}</td>
+      <td>{group}</td>
+      <td>{template.name}</td>
+      <td>{badges}</td>
+      <td>{sourceRooms}</td>
+      <td width="1%">
+        <ItemDropDown id={id} handleDelete={handleDelete} handleEdit={handleEdit} />
+      </td>
+    </tr>
   );
 };
 
@@ -46,7 +45,8 @@ Item.defaultProps = {
 
 Item.propTypes = {
   item: PropTypes.object,
-  handleDelete: PropTypes.func.isRequired
+  handleDelete: PropTypes.func.isRequired,
+  handleEdit: PropTypes.func.isRequired
 };
 
 export default Item;
