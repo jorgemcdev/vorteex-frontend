f6c2a895a392ae936653924861a7a2867ca3b8bd
Jorge Carvalho <jorgecarvalhodpt@gmail.com>
2019-03-11 19:24:03 +0000

    Remove async validation

--- src/components/instances/new/instancesSchema.js
+++ src/components/instances/new/instancesSchema.js
@@ -1,37 +1,16 @@
 import * as Yup from 'yup';
 
-import api from '../../../api';
-const e = api.endpoints;
-
 const Schema = Yup.object().shape({
   module: Yup
     .string()
     .required('Required'),
   name: Yup
     .string()
-    .required('Required')
-    .test('name', 'Name is in Use', async (value) => {
-      let res;
-      try {
-        res = await api.request.get(e.INSTANCES, null, `name=${value}`);
-      } catch (error) {
-        console.error(error);
-        return false;
-      }
-      return res.data.length === 0 && true;
-    }),
+    .required('Required'),
   codename: Yup
     .string()
-    .required('Required')
-    .test('codename', 'Codename is in Use', async (value) => {
-      let res;
-      try {
-        res = await api.request.get(e.INSTANCES, null, `codename=${value}`);
-      } catch (error) {
-        return false;
-      }
-      return res.data.length === 0 && true;
-    }),
+    .required('Required'),
+
   description: Yup
     .string()
     .required('Required'),
